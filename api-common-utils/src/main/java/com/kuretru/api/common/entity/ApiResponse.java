0bc0c5325ec7f789ac9df220500d288c83ea0074
呉真 <kuretru@gmail.com>
2019-03-13 16:04:38 +0800

    修改：使用泛型代替Object

--- api-common-utils/src/main/java/com/kuretru/api/common/entity/ApiResponse.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/entity/ApiResponse.java
@@ -6,36 +6,36 @@ import lombok.Data;
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
 @Data
-public class ApiResponse {
+public class ApiResponse<D> {
 
     private Integer code;
     private String message;
-    private Object data;
+    private D data;
 
     public ApiResponse() {
         super();
     }
 
-    public ApiResponse(Integer code, String message, Object data) {
+    public ApiResponse(Integer code, String message, D data) {
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
     public static ApiResponse success(Object data) {
-        return new ApiResponse(2000, "success", data);
+        return new ApiResponse<>(2000, "success", data);
     }
 
     public static ApiResponse created(Object data) {
-        return new ApiResponse(2001, "created", data);
+        return new ApiResponse<>(2001, "created", data);
     }
 
     public static ApiResponse updated(Object data) {
-        return new ApiResponse(2002, "updated", data);
+        return new ApiResponse<>(2002, "updated", data);
     }
 
     public static ApiResponse deleted(Object data) {
-        return new ApiResponse(2003, "deleted", data);
+        return new ApiResponse<>(2003, "deleted", data);
     }
 
 }
