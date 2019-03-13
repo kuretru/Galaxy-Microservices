33f4670ff20e810edb8006a2d25953abb050837d
呉真 <kuretru@gmail.com>
2019-03-13 18:22:51 +0800

    修改：指定主键类型

--- api-common-utils/src/main/java/com/kuretru/api/common/entity/data/BaseDO.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/entity/data/BaseDO.java
@@ -1,5 +1,6 @@
 package com.kuretru.api.common.entity.data;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kuretru.api.common.util.InstantUtils;
@@ -13,12 +14,17 @@ import java.time.Instant;
 @Data
 public abstract class BaseDO {
 
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     @JsonFormat(pattern = InstantUtils.GENERAL_DATE_FORMAT, timezone = InstantUtils.GENERAL_TIME_ZONE)
     private Instant createAt;
 
     private Instant modifiedAt;
+
+    public void addCrateTime() {
+        createAt = Instant.now();
+        modifiedAt = Instant.now();
+    }
 
 }
