d647442c1075968f246e75f74c4c5427f4821cad
呉真 <kuretru@gmail.com>
2019-03-25 16:44:46 +0800

    新增：关闭服务的方法

--- api-common-utils/src/main/java/com/kuretru/api/common/controller/TestController.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/controller/TestController.java
@@ -2,8 +2,12 @@ package com.kuretru.api.common.controller;
 
 import com.kuretru.api.common.entity.ApiResponse;
 import com.kuretru.api.common.util.InstantUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +20,30 @@ import java.time.Instant;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class TestController extends BaseController {
 
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    public TestController(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @GetMapping("/ping")
     public ApiResponse ping() {
         String now = InstantUtils.instantToString(Instant.now());
         return ApiResponse.success(now);
+    }
+
+    @PostMapping("/shutdown")
+    public ApiResponse shutdown() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            SpringApplication.exit(applicationContext);
+        }, "shutdown-thread").start();
+        return ApiResponse.success("关闭中。");
     }
 
 }
