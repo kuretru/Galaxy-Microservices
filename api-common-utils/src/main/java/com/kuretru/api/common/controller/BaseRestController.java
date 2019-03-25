dbe54791ec13643d3893d6312f5d349058c773e7
呉真 <kuretru@gmail.com>
2019-03-25 13:09:39 +0800

    新增：通用控制器操作

--- api-common-utils/src/main/java/com/kuretru/api/common/controller/BaseRestController.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/controller/BaseRestController.java
@@ -1,27 +1,44 @@
 package com.kuretru.api.common.controller;
 
-import lombok.Getter;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import com.kuretru.api.common.entity.ApiResponse;
+import com.kuretru.api.common.exception.ApiException;
+import com.kuretru.api.common.exception.NotFoundException;
+import com.kuretru.api.common.service.BaseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
-@Getter
-public abstract class BaseRestController {
+public abstract class BaseRestController<S extends BaseService> extends BaseController {
 
-    protected HttpServletRequest request;
-    protected HttpServletResponse response;
-    protected HttpSession session;
+    protected S service;
 
-    @ModelAttribute
-    public void init(HttpServletRequest request, HttpServletResponse response) {
-        this.request = request;
-        this.response = response;
-        this.session = request.getSession();
+    /**
+     * 注入对应serice实例
+     *
+     * @param service service实例
+     */
+    public abstract void setService(S service);
+
+    @GetMapping("/{id}")
+    public ApiResponse get(@PathVariable("id") Long id) throws ApiException {
+        Object result = service.get(id);
+        if (result == null) {
+            throw new NotFoundException("未找到相关对象！");
+        }
+        return ApiResponse.success(result);
+    }
+
+    @GetMapping
+    public ApiResponse list() throws ApiException {
+        List result = service.list();
+        if (result.isEmpty()) {
+            throw new NotFoundException("未找到相关对象！");
+        }
+        return ApiResponse.success(result);
     }
 
 }
