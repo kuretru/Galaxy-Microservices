33cd445ec6c0fbad3dd2a015e20634e30ed2af3d
呉真 <kuretru@gmail.com>
2019-03-27 14:35:47 +0800

    新增：文件上传DTO

---
+++ api-common-utils/src/main/java/com/kuretru/api/common/entity/transfer/FileUploadDTO.java
@@ -0,0 +1,15 @@
+package com.kuretru.api.common.entity.transfer;
+
+import lombok.Data;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+@Data
+public class FileUploadDTO {
+
+    private String fileName;
+
+    private String url;
+
+}
