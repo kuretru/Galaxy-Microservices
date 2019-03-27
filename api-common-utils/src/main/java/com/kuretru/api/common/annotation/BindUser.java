4631289dd30d939885356446016b9c1330795004
呉真 <kuretru@gmail.com>
2019-03-27 19:48:13 +0800

    新增：BindUser注解，及相应切面，自动绑定用户ID

---
+++ api-common-utils/src/main/java/com/kuretru/api/common/annotation/BindUser.java
@@ -0,0 +1,13 @@
+package com.kuretru.api.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 呉真 Kuretru < kuretru@gmail.com >
+ */
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BindUser {
+
+}
--- api-common-utils/src/main/java/com/kuretru/api/common/aspect/AuthorizationAspect.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/aspect/AuthorizationAspect.java
@@ -1,19 +1,25 @@
 package com.kuretru.api.common.aspect;
 
+import com.kuretru.api.common.annotation.BindUser;
 import com.kuretru.api.common.annotation.RequestAuthorization;
 import com.kuretru.api.common.configuration.GeneralConstants;
 import com.kuretru.api.common.entity.business.AccessTokenBO;
-import com.kuretru.api.common.exception.ApiException;
+import com.kuretru.api.common.entity.enums.UserRoleEnum;
 import com.kuretru.api.common.exception.AuthenticationFailedException;
 import com.kuretru.api.common.manager.AccessTokenManager;
 import com.kuretru.api.common.util.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
@@ -33,23 +39,54 @@ public class AuthorizationAspect {
         this.accessTokenManager = accessTokenManager;
     }
 
-    @Before("@annotation(requestAuthorization)")
-    public void before(RequestAuthorization requestAuthorization) throws ApiException {
+    @Around("@annotation(requestAuthorization)")
+    public Object around(ProceedingJoinPoint joinPoint, RequestAuthorization requestAuthorization) throws Throwable {
         String header = request.getHeader(GeneralConstants.ACCESS_TOKEN_HEADER);
 
         // 1.判断是否有请求头
         if (StringUtils.isNullOrEmpty(header)) {
             throw new AuthenticationFailedException("缺少请求头" + GeneralConstants.ACCESS_TOKEN_HEADER);
         }
-        // 2.万能Token
+
+        AccessTokenBO tokenBO;
         if (GeneralConstants.MAGIC_TOKEN.equals(header)) {
-            return;
+            // 2.万能Token
+            tokenBO = AccessTokenBO.build(1L, UserRoleEnum.ADMIN);
+        } else {
+            // 2.正常查询Redis
+            tokenBO = accessTokenManager.verifyToken(header);
         }
+
         // 3.验证角色是否有该权限
-        AccessTokenBO model = accessTokenManager.verifyToken(header);
-        if (requestAuthorization.requiredRole().getCode() > model.getRole().getCode()) {
+        if (requestAuthorization.requiredRole().getCode() > tokenBO.getRole().getCode()) {
             throw new AuthenticationFailedException("该用户没有操作此接口的权限");
         }
+
+        // 4.获取添加用户ID后的入参
+        Object[] args = bindUser(tokenBO, joinPoint);
+        return joinPoint.proceed(args);
+    }
+
+    private Object[] bindUser(AccessTokenBO tokenBO, ProceedingJoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        Parameter[] parameters = method.getParameters();
+        for (int i = 0; i < parameters.length; i++) {
+            Annotation[] annotations = parameters[i].getAnnotations();
+            for (Annotation annotation : annotations) {
+                if (!(annotation instanceof BindUser)) {
+                    continue;
+                }
+                // 如果绑定对象是Long的情况
+                if (parameters[i].getType().equals(Long.class)) {
+                    args[i] = tokenBO.getId();
+                }
+                // TODO 如果绑定对象是其他POJO的情况
+            }
+        }
+        return args;
     }
 
 }
