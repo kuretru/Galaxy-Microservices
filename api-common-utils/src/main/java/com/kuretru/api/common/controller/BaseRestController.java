b0ef571a3dc47fb65c548de04d9693363c2c5805
呉真 <kuretru@gmail.com>
2019-03-28 15:35:55 +0800

    修改：创建时返回的状态码

--- api-common-utils/src/main/java/com/kuretru/api/common/controller/BaseRestController.java
+++ api-common-utils/src/main/java/com/kuretru/api/common/controller/BaseRestController.java
@@ -5,10 +5,8 @@ import com.kuretru.api.common.entity.ApiResponse;
 import com.kuretru.api.common.exception.ApiException;
 import com.kuretru.api.common.exception.NotFoundException;
 import com.kuretru.api.common.service.BaseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,9 +46,10 @@ public abstract class BaseRestController<S extends BaseService<?, ?, T>, T> exte
 
     @RequestAuthorization
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse create(@RequestBody T record) throws ApiException {
         T result = service.save(record);
-        return ApiResponse.success(result);
+        return ApiResponse.created(result);
     }
 
 }
