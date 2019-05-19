c3421c2fd957978bd1dc7ec91c48211e3cf84c14
呉真 <kuretru@gmail.com>
2019-05-19 11:49:04 +0800

    新增：将基础模板控制器进一步提取

--- src/main/java/com/kuretru/api/common/controller/BaseRestController.java
+++ src/main/java/com/kuretru/api/common/controller/BaseRestController.java
@@ -4,19 +4,14 @@ import com.kuretru.api.common.annotation.RequestAuthorization;
 import com.kuretru.api.common.entity.ApiResponse;
 import com.kuretru.api.common.entity.transfer.BaseDTO;
 import com.kuretru.api.common.exception.ApiException;
-import com.kuretru.api.common.exception.NotFoundException;
 import com.kuretru.api.common.service.BaseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 /**
  * @author 呉真 Kuretru < kuretru@gmail.com >
  */
-public abstract class BaseRestController<S extends BaseService<?, ?, T>, T extends BaseDTO> extends BaseController {
-
-    protected S service;
+public abstract class BaseRestController<S extends BaseService<?, ?, T>, T extends BaseDTO> extends BaseCrudController<S, T> {
 
     /**
      * 子类中必须调用此构造函数，传递参数
@@ -36,50 +31,43 @@ public abstract class BaseRestController<S extends BaseService<?, ?, T>, T exten
      * @param service 对应的Service
      */
     public BaseRestController(S service) {
-        this.service = service;
+        super(service);
     }
 
     @RequestAuthorization
     @GetMapping("/{id}")
+    @Override
     public ApiResponse get(@PathVariable("id") Long id) throws ApiException {
-        T result = service.get(id);
-        if (result == null) {
-            throw new NotFoundException("未找到相关对象！");
-        }
-        return ApiResponse.success(result);
+        return super.get(id);
     }
 
     @RequestAuthorization
     @GetMapping
+    @Override
     public ApiResponse list() throws ApiException {
-        List<T> result = service.list();
-        if (result.isEmpty()) {
-            throw new NotFoundException("未找到相关对象！");
-        }
-        return ApiResponse.success(result);
+        return super.list();
     }
 
     @RequestAuthorization
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Override
     public ApiResponse create(@RequestBody T record) throws ApiException {
-        T result = service.save(record);
-        return ApiResponse.created(result);
+        return super.create(record);
     }
 
     @RequestAuthorization
     @PutMapping("/{id}")
+    @Override
     public ApiResponse update(@PathVariable("id") Long id, @RequestBody T record) throws ApiException {
-        record.setId(id);
-        T result = service.update(record);
-        return ApiResponse.updated(result);
+        return super.update(id, record);
     }
 
     @RequestAuthorization
     @DeleteMapping("/{id}")
+    @Override
     public ApiResponse remove(@PathVariable("id") Long id) throws ApiException {
-        service.remove(id);
-        return ApiResponse.removed();
+        return super.remove(id);
     }
 
 }
