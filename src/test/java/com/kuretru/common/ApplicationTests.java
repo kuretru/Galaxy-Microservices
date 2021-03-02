e0f959c6a81cbc832a0261a509b94e3e185f9ac9
呉真 <kuretru@gmail.com>
2021-03-02 16:04:51 +0800

    构建：新增项目目录结构

---
+++ src/test/java/com/kuretru/common/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuretru.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
