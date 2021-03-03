b574a7059d6059e5740f8874dc31c4a38da5af47
呉真 <kuretru@gmail.com>
2021-03-03 20:37:05 +0800

    新增：类与字段的注释

--- src/main/java/com/kuretru/common/entity/BaseDO.java
+++ src/main/java/com/kuretru/common/entity/BaseDO.java
@@ -7,18 +7,24 @@ import lombok.Data;
 import java.time.Instant;
 
 /**
+ * 数据对象父抽象类
+ *
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
 @Data
 public abstract class BaseDO {
 
+    /** 物理主键，自增 */
     @TableId(type = IdType.AUTO)
     private Long id;
 
+    /** 业务逻辑主键 */
     private String uuid;
 
+    /** 记录主动创建的时刻 */
     private Instant createTime;
 
+    /** 记录上一次被动更新的时刻 */
     private Instant updateTime;
 
 }
