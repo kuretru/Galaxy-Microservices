94bc8568a131169fb0a050ba18907f21c25d0ab6
呉真 <kuretru@gmail.com>
2021-03-04 18:52:46 +0800

    新增：数据传输对象父抽象类

--- src/main/java/com/kuretru/common/entity/data/BaseDO.java
+++ src/main/java/com/kuretru/common/entity/data/BaseDO.java
@@ -1,4 +1,4 @@
-package com.kuretru.common.entity;
+package com.kuretru.common.entity.data;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
---
+++ src/main/java/com/kuretru/common/entity/transfer/BaseDTO.java
@@ -0,0 +1,18 @@
+package com.kuretru.common.entity.transfer;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+/**
+ * 数据传输对象父抽象类
+ *
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@Data
+public abstract class BaseDTO {
+
+    /** 业务逻辑主键 */
+    private UUID id;
+
+}
