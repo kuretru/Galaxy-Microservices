abd6b00229c9db13dc9b3a3cc2345b7996aa7da1
呉真 <kuretru@gmail.com>
2021-03-08 16:49:40 +0800

    新增：日志切面

---
+++ src/main/java/com/kuretru/api/common/constant/LoggingConstants.java
@@ -0,0 +1,17 @@
+package com.kuretru.api.common.constant;
+
+/**
+ * 日志相关的常量类
+ *
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+public class LoggingConstants {
+
+    /** 日中系统中获取TraceId使用的键 */
+    public static final String TRACE_ID_KEY = "traceId";
+
+    private LoggingConstants() {
+
+    }
+
+}
