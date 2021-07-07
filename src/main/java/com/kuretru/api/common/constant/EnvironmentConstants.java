3189eb667e28febb43ad2905f5d5dfe0df049ac0
呉真 <kuretru@gmail.com>
2021-07-07 09:25:01 +0800

    新增：开发环境配置CORS

---
+++ src/main/java/com/kuretru/api/common/constant/EnvironmentConstants.java
@@ -0,0 +1,23 @@
+package com.kuretru.api.common.constant;
+
+/**
+ * 启动环境相关的常量类
+ *
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+public class EnvironmentConstants {
+
+    /** 开发环境 */
+    public static final String DEVELOPMENT = "development";
+
+    /** 测试环境 */
+    public static final String STAGE = "stage";
+
+    /** 生产环境 */
+    public static final String PRODUCTION = "production";
+
+    private EnvironmentConstants() {
+
+    }
+
+}
