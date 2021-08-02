b9a0c6970b6257e2c13b1272b451b5c053684109
呉真 <kuretru@gmail.com>
2021-08-02 10:09:00 +0800

    重构：将方法统一放至工具类

--- src/main/java/com/kuretru/api/common/util/EnumUtils.java
+++ src/main/java/com/kuretru/api/common/util/EnumUtils.java
@@ -2,6 +2,9 @@ package com.kuretru.api.common.util;
 
 import com.kuretru.api.common.entity.enums.BaseEnum;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 枚举相关工具类
  *
@@ -21,7 +24,7 @@ public class EnumUtils {
      * @param <E>    派生于BaseEnum的枚举类型
      * @return 枚举实体
      */
-    public static <E extends BaseEnum> E valueOf(Class<E> eClass, Short code) {
+    public static <E extends BaseEnum<E>> E valueOf(Class<E> eClass, Short code) {
         if (code == null) {
             throw new NullPointerException("枚举编号为null");
         }
@@ -41,7 +44,7 @@ public class EnumUtils {
      * @param <E>    派生于BaseEnum的枚举类型
      * @return 枚举实体
      */
-    public static <E extends BaseEnum> E valueOf(Class<E> eClass, String value) {
+    public static <E extends BaseEnum<E>> E valueOf(Class<E> eClass, String value) {
         if (value == null) {
             throw new NullPointerException("枚举内容为null");
         }
@@ -51,6 +54,21 @@ public class EnumUtils {
             }
         }
         throw new IllegalArgumentException("不存在枚举内容所对应的枚举实体");
+    }
+
+    /**
+     * 将枚举转换成键值对的形式
+     *
+     * @param values 枚举
+     * @param <E>    派生自BaseEnum的枚举类
+     * @return 键值对形式的枚举
+     */
+    public static <E extends BaseEnum<E>> Map<BaseEnum<E>, String> buildEnumMap(BaseEnum<E>[] values) {
+        Map<BaseEnum<E>, String> result = new TreeMap<>();
+        for (BaseEnum<E> value : values) {
+            result.put(value, value.getValue());
+        }
+        return result;
     }
 
 }
