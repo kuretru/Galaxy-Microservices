582d425fe61f608f60d744be10059b58cea0487f
呉真 <kuretru@gmail.com>
2022-03-11 17:49:32 +0800

    新增：启动时不载入DataSource

--- src/main/java/com/kuretru/api/common/Application.java
+++ src/main/java/com/kuretru/api/common/Application.java
@@ -2,11 +2,12 @@ package com.kuretru.api.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
