6bde539306998a9afec0b00e756642fcfd6636e9
呉真 <kuretru@gmail.com>
2022-03-13 18:09:42 +0800

    更新：Context-Path

--- src/main/java/com/kuretru/api/common/controller/TestController.java
+++ src/main/java/com/kuretru/api/common/controller/TestController.java
@@ -5,7 +5,6 @@ import com.kuretru.api.common.util.InstantUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -16,7 +15,6 @@ import java.time.Instant;
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
 @RestController
-@RequestMapping("/api")
 @Tag(name = "TestController", description = "测试控制器")
 public class TestController extends BaseController {
 
