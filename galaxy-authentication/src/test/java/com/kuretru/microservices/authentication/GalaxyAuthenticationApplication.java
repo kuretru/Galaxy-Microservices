90d3fd8f9b5a67ab52f15ca3b00b3b09d7b53b4c
呉真 <kuretru@gmail.com>
2022-03-28 14:08:16 +0800

    构建(authentication)：修复构建失败的问题

--- galaxy-authentication/src/test/java/com/kuretru/microservices/authentication/GalaxyAuthenticationApplication.java
+++ galaxy-authentication/src/test/java/com/kuretru/microservices/authentication/GalaxyAuthenticationApplication.java
@@ -2,11 +2,12 @@ package com.kuretru.microservices.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GalaxyAuthenticationApplication {
 
     public static void main(String[] args) {
