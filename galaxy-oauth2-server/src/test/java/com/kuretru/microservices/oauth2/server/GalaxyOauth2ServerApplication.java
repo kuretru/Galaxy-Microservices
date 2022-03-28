73bb131599345b74099b8d18bda13b9a4c507f81
呉真 <kuretru@gmail.com>
2022-03-28 13:36:30 +0800

    构建(oauth2-server)：新增项目结构

---
+++ galaxy-oauth2-server/src/test/java/com/kuretru/microservices/oauth2/server/GalaxyOauth2ServerApplication.java
@@ -0,0 +1,16 @@
+package com.kuretru.microservices.oauth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootApplication
+public class GalaxyOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxyOauth2ServerApplication.class, args);
+    }
+
+}
---
+++ galaxy-oauth2-server/src/test/java/com/kuretru/microservices/oauth2/server/GalaxyOauth2ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuretru.microservices.oauth2.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootTest
+class GalaxyOauth2ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
