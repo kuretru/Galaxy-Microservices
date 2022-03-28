5cf3a69b3f1f311403b08962ae8641e0c78cc521
呉真 <kuretru@gmail.com>
2022-03-28 13:39:52 +0800

    构建(authentication)：新增项目结构

---
+++ galaxy-authentication/src/test/java/com/kuretru/microservices/authentication/GalaxyAuthenticationApplication.java
@@ -0,0 +1,16 @@
+package com.kuretru.microservices.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootApplication
+public class GalaxyAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalaxyAuthenticationApplication.class, args);
+    }
+
+}
---
+++ galaxy-authentication/src/test/java/com/kuretru/microservices/authentication/GalaxyAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuretru.microservices.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@SpringBootTest
+class GalaxyAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
