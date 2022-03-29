4c1e3650b0b23675af83ea089d6f67ac10622a91
呉真 <kuretru@gmail.com>
2022-03-29 14:56:47 +0800

    更新(web)：AutoConfiguration方式

--- galaxy-web/src/main/java/com/kuretru/microservices/web/configuration/GalaxyWebAutoConfiguration.java
+++ galaxy-web/src/main/java/com/kuretru/microservices/web/configuration/GalaxyWebAutoConfiguration.java
@@ -1,35 +1,13 @@
 package com.kuretru.microservices.web.configuration;
 
-import com.kuretru.microservices.web.aspect.LoggingAspect;
-import com.kuretru.microservices.web.controller.ExceptionController;
-import com.kuretru.microservices.web.controller.TestController;
-import com.kuretru.microservices.web.manager.impl.TraceIdManagerImpl;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
 @Configuration
-@Import({CorsConfiguration.class, MybatisPlusConfiguration.class})
+@ComponentScan("com.kuretru.microservices.web")
 public class GalaxyWebAutoConfiguration {
-
-    @Configuration
-    @Import(LoggingAspect.class)
-    protected static class AspectConfiguration {
-
-    }
-
-    @Configuration
-    @Import({ExceptionController.class, TestController.class})
-    protected static class ControllerConfiguration {
-
-    }
-
-    @Configuration
-    @Import(TraceIdManagerImpl.class)
-    protected static class ManagerConfiguration {
-
-    }
 
 }
