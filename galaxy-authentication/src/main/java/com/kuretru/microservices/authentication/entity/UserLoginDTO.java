02526a8e1ee3fd59d54fc26dd0f65adf52a355a4
呉真 <kuretru@gmail.com>
2022-04-01 16:12:09 +0800

    新增(galaxy-authentication)：用户登录响应实体

---
+++ galaxy-authentication/src/main/java/com/kuretru/microservices/authentication/entity/UserLoginDTO.java
@@ -0,0 +1,25 @@
+package com.kuretru.microservices.authentication.entity;
+
+import com.kuretru.microservices.authentication.entity.AccessTokenDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+/**
+ * @author 呉真(kuretru) <kuretru@gmail.com>
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+
+    @NotNull
+    private UUID userId;
+
+    @NotNull
+    private AccessTokenDTO accessToken;
+
+}
