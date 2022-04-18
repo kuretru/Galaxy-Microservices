ce44dcf0d64272a976f17bf59534edc07ca34c2a
呉真 <kuretru@gmail.com>
2022-04-18 23:49:26 +0800

    修复(oauth2-client)：单元测试失败的问题

--- galaxy-oauth2-client/src/test/java/com/kuretru/microservices/oauth2/client/OAuth2ClientApplication.java
+++ galaxy-oauth2-client/src/test/java/com/kuretru/microservices/oauth2/client/OAuth2ClientApplication.java
@@ -1,12 +1,19 @@
 package com.kuretru.microservices.oauth2.client;
 
+import com.kuretru.microservices.oauth2.client.galaxy.GalaxyClientProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackageClasses = GalaxyClientProperty.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.kuretru.microservices.oauth2.client.*")
+})
 public class OAuth2ClientApplication {
 
     public static void main(String[] args) {
