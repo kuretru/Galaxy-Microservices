ef058145cac387d40f1f1bfe91e9f34ccb922ef1
呉真 <kuretru@gmail.com>
2023-01-26 22:04:59 +0800

    删除(authentication)：@NotNull注解

--- galaxy-authentication/src/main/java/com/kuretru/microservices/authentication/interceptor/AuthenticationInterceptor.java
+++ galaxy-authentication/src/main/java/com/kuretru/microservices/authentication/interceptor/AuthenticationInterceptor.java
@@ -3,7 +3,6 @@ package com.kuretru.microservices.authentication.interceptor;
 import com.kuretru.microservices.authentication.constant.AccessTokenConstants;
 import com.kuretru.microservices.authentication.context.AccessTokenContext;
 import com.kuretru.microservices.authentication.entity.AccessTokenDTO;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -18,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         AccessTokenContext.removeAccessToken();
         String id = request.getHeader(AccessTokenConstants.ACCESS_TOKEN_ID_HEADER);
         if (StringUtils.hasText(id)) {
