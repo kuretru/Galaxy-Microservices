43eca3ff23e57ac995dfaea254ef0836f604298e
呉真 <kuretru@gmail.com>
2026-05-26 22:30:34 +0800

    Feature(common): 枚举值增加children

--- galaxy-common/src/main/java/com/kuretru/microservices/common/entity/enums/EnumDTO.java
+++ galaxy-common/src/main/java/com/kuretru/microservices/common/entity/enums/EnumDTO.java
@@ -1,15 +1,16 @@
 package com.kuretru.microservices.common.entity.enums;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * @author 呉真(kuretru) <kuretru@gmail.com>
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class EnumDTO {
 
     /** 前端展示的值 */
@@ -17,5 +18,14 @@ public class EnumDTO {
 
     /** 前端保存的值 */
     private String value;
+
+    /** 子元素 */
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<EnumDTO> children;
+
+    public EnumDTO(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
 
 }
